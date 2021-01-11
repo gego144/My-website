44de95151a3a78c4c6c01dbf4f14c17c3b94b56a
gego144 <56304474+gego144@users.noreply.github.com>
2021-01-11 04:58:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,29 +5,7 @@ var modalBtn = document.getElementById('modalBtn');
 // Get close button
 var closeBtn = document.getElementsByClassName('closeBtn')[0];
 
-// Listen for open click
-modalBtn.addEventListener('click', openModal);
-// Listen for close click
-closeBtn.addEventListener('click', closeModal);
-// Listen for outside click
-window.addEventListener('click', outsideClick);
-
-// Function to open modal
-function openModal() {
-  modal.style.display = 'block';
-}
-
-// Function to close modal
-function closeModal() {
-  modal.style.display = 'none';
-}
-
-// Close If Outside Click
-function outsideClick(e) {
-  if (e.target == modal) {
-    modal.style.display = 'none';
-  }
-}
+createmodal(modal, modalBtn, closeBtn);
 
 // Get modal element
 var modal2 = document.getElementById('simpleModal2');
@@ -36,29 +14,7 @@ var modalBtn2 = document.getElementById('modalBtn2');
 // Get close button
 var closeBtn2 = document.getElementsByClassName('closeBtn')[1];
 
-// Listen for open click
-modalBtn2.addEventListener('click', openModal2);
-// Listen for close click
-closeBtn2.addEventListener('click', closeModal2);
-// Listen for outside click
-window.addEventListener('click', outsideClick2);
-
-// Function to open modal
-function openModal2() {
-  modal2.style.display = 'block';
-}
-
-// Function to close modal
-function closeModal2() {
-  modal2.style.display = 'none';
-}
-
-// Close If Outside Click
-function outsideClick2(e) {
-  if (e.target == modal2) {
-    modal2.style.display = 'none';
-  }
-}
+createmodal(modal2, modalBtn2, closeBtn2);
 
 // Get modal element
 var modal3 = document.getElementById('simpleModal3');
@@ -67,29 +23,7 @@ var modalBtn3 = document.getElementById('modalBtn3');
 // Get close button
 var closeBtn3 = document.getElementsByClassName('closeBtn')[2];
 
-// Listen for open click
-modalBtn3.addEventListener('click', openModal3);
-// Listen for close click
-closeBtn3.addEventListener('click', closeModal3);
-// Listen for outside click
-window.addEventListener('click', outsideClick3);
-
-// Function to open modal
-function openModal3() {
-  modal3.style.display = 'block';
-}
-
-// Function to close modal
-function closeModal3() {
-  modal3.style.display = 'none';
-}
-
-// Close If Outside Click
-function outsideClick3(e) {
-  if (e.target == modal3) {
-    modal3.style.display = 'none';
-  }
-}
+createmodal(modal3, modalBtn3, closeBtn3);
 
 // Get modal element
 var modal4 = document.getElementById('simpleModal4');
@@ -98,29 +32,7 @@ var modalBtn4 = document.getElementById('modalBtn4');
 // Get close button
 var closeBtn4 = document.getElementsByClassName('closeBtn')[3];
 
-// Listen for open click
-modalBtn4.addEventListener('click', openModal4);
-// Listen for close click
-closeBtn4.addEventListener('click', closeModal4);
-// Listen for outside click
-window.addEventListener('click', outsideClick4);
-
-// Function to open modal
-function openModal4() {
-  modal4.style.display = 'block';
-}
-
-// Function to close modal
-function closeModal4() {
-  modal4.style.display = 'none';
-}
-
-// Close If Outside Click
-function outsideClick4(e) {
-  if (e.target == modal4) {
-    modal4.style.display = 'none';
-  }
-}
+createmodal(modal4, modalBtn4, closeBtn4);
 
 
 var modal5 = document.getElementById('simpleModal5');
@@ -129,26 +41,44 @@ var modalBtn5 = document.getElementById('modalBtn5');
 // Get close button
 var closeBtn5 = document.getElementsByClassName('closeBtn')[4];
 
-// Listen for open click
-modalBtn5.addEventListener('click', openModal5);
-// Listen for close click
-closeBtn5.addEventListener('click', closeModal5);
-// Listen for outside click
-window.addEventListener('click', outsideClick5);
+createmodal(modal5, modalBtn5, closeBtn5);
 
-// Function to open modal
-function openModal5() {
-  modal5.style.display = 'block';
-}
 
-// Function to close modal
-function closeModal5() {
-  modal5.style.display = 'none';
-}
 
-// Close If Outside Click
-function outsideClick5(e) {
-  if (e.target == modal5) {
+var modal6 = document.getElementById('simpleModal6');
+
+var modalBtn6 = document.getElementById('modalBtn6');
+
+var closeBtn6 = document.getElementsByClassName('closeBtn')[5];
+
+createmodal(modal6, modalBtn6, closeBtn6);
+
+
+
+function createmodal(modal, modalbtn, closebtn){
+  // Listen for open click
+  modalbtn.addEventListener('click', open_modal);
+  // Listen for close click
+  closebtn.addEventListener('click', close_modal);
+  // Listen for outside click
+  window.addEventListener('click', outside_click);
+
+  // Function to open modal
+  function open_modal() {
+    modal.style.display = 'block';
+  }
+
+  // Function to close modal
+  function close_modal() {
     modal5.style.display = 'none';
   }
+
+  // Close If Outside Click
+  function outside_click(e) {
+    if (e.target == modal) {
+      modal.style.display = 'none';
+    }
+  }
+
+
 }
